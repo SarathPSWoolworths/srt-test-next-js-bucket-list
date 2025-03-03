fec961068990319f5adb10d543df91cb6e7d47fd
Sarath PS <sps@tcs.woolworths.com.au>
2025-03-03 16:01:30 +0530

    Refactor layout component by removing unused font imports and simplifying the body structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,19 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Srt Testing Next JS",
-  description: "Generated using create next app by sarath",
+  title: 'Srt Testing Next JS',
+  description: 'Generated using create next app by sarath'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
